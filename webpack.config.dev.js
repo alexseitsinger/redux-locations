b216bdeb051b3ccb302340ad914e8576ea032b4b
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-12-05 14:40:57 -0500

    chore: Updates webpack config.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,15 +1,15 @@
 const path = require("path")
 const nodeExternals = require("webpack-node-externals")
-const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = {
   entry: "./src/index.js",
-  mode: "production",
+  mode: "development",
   target: "node",
-  devtool: false,
+  devtool: "source-map",
   output: {
     path: path.resolve("./dist"),
-    filename: "[name].js",
+    filename: "[name].dev.js",
+    sourceMapFilename: "[name].dev.js.map",
     libraryTarget: "commonjs2",
   },
   module: {
@@ -18,23 +18,8 @@ module.exports = {
         test: /\.jsx?$/,
         include: [path.resolve("./src")],
         use: "babel-loader"
-      },
-    ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-        sourceMap: false,
-        terserOptions: {
-          warnings: false,
-          output: {
-            comments: false,
-          },
-        },
-      }),
-    ],
+      }
+    ]
   },
   externals: [
     nodeExternals({
