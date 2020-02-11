0cdce3f88bae8a68076653d68ba147d53d2dc4e7
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-02-11 02:14:49 -0500

    fix: Changes action type check.

--- src/locations/middleware.ts
+++ src/locations/middleware.ts
@@ -1,5 +1,5 @@
-import { Location as HistoryLocation } from "history"
 import { AnyAction, Dispatch } from "redux"
+import { Location as HistoryLocation } from "history"
 
 import { updateLocations } from "./actions"
 import { LOCATION_CHANGE } from "./constants"
@@ -10,11 +10,7 @@ let lastLocation: HistoryLocation = defaultLocation
 export const createLocationsMiddleware = (
   actionType = LOCATION_CHANGE
 ) => () => (next: Dispatch) => (action: AnyAction): ReturnType<typeof next> => {
-  if (action.type === undefined) {
-    return next(action)
-  }
-
-  if (action.type === actionType) {
+  if (action.type !== undefined && action.type === actionType) {
     const currentLocation = action.payload.location
     // If we use store.dispatch instead, the router will spam location changes
     // (4x). So, to make sure it only happens the once, we need to silently run
